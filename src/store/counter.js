5ca899b5aabb55add6475297512f38f17745ea8b
jigooo <h00kk@naver.com>
2023-08-22 17:01:44 +0900

    practice react-redux & redux toolkit

---
+++ src/store/counter.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export const counterActions = counterSlice.actions;
+
+//slice 전체가 아니라 reducer만 export 해준다.
+export default counterSlice.reducer;
